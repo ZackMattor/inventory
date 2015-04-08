4fd03d4e35b6592fa157896526d358bd9de45de0
Mike George <mike@tallduck.com>
2015-04-07 22:09:19 -0400

    lets add some firebase

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'inventory',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' wss://*.firebaseio.com" },
+    firebase: 'https://inventory-mg.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
